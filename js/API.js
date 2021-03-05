3ac1635c608c658f839d1854c2be234d396c3a20
Florencia Singh <florencia.singh@gmail.com>
2021-03-05 18:58:52 -0300

    initial commit

---
+++ js/API.js
@@ -0,0 +1,13 @@
+const apiKey = "9Kj5g3A9tVMhZr6HUCRe2zGawrHFutqZ";
+
+const searchEndpoint = "https://api.giphy.com/v1/gifs/search";
+
+const trendingEndpoint = "https://api.giphy.com/v1/gifs/trending";
+
+const trendingTagsEndpoint = "https://api.giphy.com/v1/trending/searches";
+
+const searchAutocomplete = "https://api.giphy.com/v1/gifs/search/tags";
+
+const uploadGifEndpoint = "https://upload.giphy.com/v1/gifs";
+
+const getGifByIdEndpoint = "https://api.giphy.com/v1/gifs";
